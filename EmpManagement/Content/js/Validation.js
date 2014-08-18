1c99eefe15dfdd3c3a9ab887ec55c434d791a6d2
Ripal <Ripal@Ripal.promact.com>
2014-08-18 19:27:12 +0530

    commit

---
+++ EmpManagement/Content/js/Validation.js
@@ -0,0 +1,16 @@
+﻿function validation() {
+    var name = document.getElementById("Name");
+    var Dept = document.getElementById("DeptID");
+    var designation = document.getElementById("Designation");
+    var contactno = document.getElementById("ContactNo");
+    var emailid = document.getElementById("Emailid");
+
+   
+}
+var letters = /^[a-zA-Z]+$/
+if (name.match.letters)
+{
+    alert("Hello");
+    document.getElementById("name").value = "Text only";
+    document.getElementById("name").style.visibility = true;
+}
